9b1ec25eaa1e86e3faf6f156ea5a8a0631c59b05
ankush <anku0512@gmail.com>
2022-07-02 17:11:31 +0530

    unused pages removed

--- lib/index.dart
+++ lib/index.dart
@@ -4,44 +4,22 @@ export 'lessons/lessons_widget.dart' show LessonsWidget;
 export 'home/home_widget.dart' show HomeWidget;
 export 'create_post/create_post_widget.dart' show CreatePostWidget;
 export 'profile/profile_widget.dart' show ProfileWidget;
-export 'post_page_sit/post_page_sit_widget.dart' show PostPageSitWidget;
 export 'edit_profile/edit_profile_widget.dart' show EditProfileWidget;
 export 'complete_profile/complete_profile_widget.dart'
     show CompleteProfileWidget;
 export 'sit/sit_widget.dart' show SitWidget;
 export 'shake_hand/shake_hand_widget.dart' show ShakeHandWidget;
-export 'post_page_shake_hand/post_page_shake_hand_widget.dart'
-    show PostPageShakeHandWidget;
-export 'post_page_down/post_page_down_widget.dart' show PostPageDownWidget;
 export 'down/down_widget.dart' show DownWidget;
-export 'post_page_come/post_page_come_widget.dart' show PostPageComeWidget;
-export 'post_page_stay/post_page_stay_widget.dart' show PostPageStayWidget;
 export 'stay/stay_widget.dart' show StayWidget;
 export 'come/come_widget.dart' show ComeWidget;
-export 'post_page_leave_it/post_page_leave_it_widget.dart'
-    show PostPageLeaveItWidget;
-export 'post_page_touch/post_page_touch_widget.dart' show PostPageTouchWidget;
 export 'leave_it/leave_it_widget.dart' show LeaveItWidget;
 export 'touch/touch_widget.dart' show TouchWidget;
 export 'roll_over/roll_over_widget.dart' show RollOverWidget;
-export 'post_page_roll_over/post_page_roll_over_widget.dart'
-    show PostPageRollOverWidget;
 export 'spin/spin_widget.dart' show SpinWidget;
-export 'post_page_spin/post_page_spin_widget.dart' show PostPageSpinWidget;
-export 'post_page_tell_me_a_secret/post_page_tell_me_a_secret_widget.dart'
-    show PostPageTellMeASecretWidget;
 export 'tell_me_a_secret/tell_me_a_secret_widget.dart' show TellMeASecretWidget;
-export 'post_page_cross_paws/post_page_cross_paws_widget.dart'
-    show PostPageCrossPawsWidget;
 export 'cross_paws/cross_paws_widget.dart' show CrossPawsWidget;
 export 'chin_rest/chin_rest_widget.dart' show ChinRestWidget;
-export 'post_page_leg_weaves/post_page_leg_weaves_widget.dart'
-    show PostPageLegWeavesWidget;
-export 'post_page_chin_rest/post_page_chin_rest_widget.dart'
-    show PostPageChinRestWidget;
 export 'leg_weaves/leg_weaves_widget.dart' show LegWeavesWidget;
-export 'post_page_bar_jump/post_page_bar_jump_widget.dart'
-    show PostPageBarJumpWidget;
 export 'bar_jump/bar_jump_widget.dart' show BarJumpWidget;
 export 'post_page_sit_badge/post_page_sit_badge_widget.dart'
     show PostPageSitBadgeWidget;
@@ -70,23 +48,12 @@ export 'skillfull/skillfull_widget.dart' show SkillfullWidget;
 export 'charming/charming_widget.dart' show CharmingWidget;
 export 'playful/playful_widget.dart' show PlayfulWidget;
 export 'dance/dance_widget.dart' show DanceWidget;
-export 'post_page_dance/post_page_dance_widget.dart' show PostPageDanceWidget;
 export 'shy/shy_widget.dart' show ShyWidget;
-export 'post_page_shy/post_page_shy_widget.dart' show PostPageShyWidget;
 export 'hooped_arms/hooped_arms_widget.dart' show HoopedArmsWidget;
-export 'post_page_hooped_arms/post_page_hooped_arms_widget.dart'
-    show PostPageHoopedArmsWidget;
 export 'scoot/scoot_widget.dart' show ScootWidget;
-export 'post_page_scoot/post_page_scoot_widget.dart' show PostPageScootWidget;
 export 'jump_rope/jump_rope_widget.dart' show JumpRopeWidget;
-export 'post_page_jump_rope/post_page_jump_rope_widget.dart'
-    show PostPageJumpRopeWidget;
 export 'figure_8/figure8_widget.dart' show Figure8Widget;
-export 'post_page_figure_8/post_page_figure8_widget.dart'
-    show PostPageFigure8Widget;
 export 'frisbee/frisbee_widget.dart' show FrisbeeWidget;
-export 'post_page_frisbee/post_page_frisbee_widget.dart'
-    show PostPageFrisbeeWidget;
 export 'new_dog/new_dog_widget.dart' show NewDogWidget;
 export 'placework/placework_widget.dart' show PlaceworkWidget;
 export 'intro/intro_widget.dart' show IntroWidget;
@@ -105,5 +72,4 @@ export 'post_page_frisbee_badge/post_page_frisbee_badge_widget.dart'
     show PostPageFrisbeeBadgeWidget;
 export 'brush_teeth_copy/brush_teeth_copy_widget.dart'
     show BrushTeethCopyWidget;
-export 'resetpassowrd/resetpassowrd_widget.dart' show ResetpassowrdWidget;
 export 'store/store_widget.dart' show StoreWidget;
